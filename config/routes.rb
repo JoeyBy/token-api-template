208989e7c0b097fc9af4563e592bcb4586cc49aa
Joey Byrne <joey.byrne@outlook.com>
2017-05-27 21:22:26 -0700

    generate hike model, create index only, route for hike

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "hike/index"
-
   scope :format => true, :constraints => {:format => 'json'} do
   	post "/login" => "sessions#create"
   	delete "/logout" => "sessions#destroy"
+
+  	#Get all hikes in JSON
+  	get "/hike" => "hikes#index"
   end
 end
